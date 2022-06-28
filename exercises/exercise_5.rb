43d117067167dcef1ad2ad6d94db011f84619f18
robinshrestha12 <robinshrestha.maple@gmail.com>
2022-06-28 19:35:05 -0400

    Exercise5 complete

--- exercises/exercise_5.rb
+++ exercises/exercise_5.rb
@@ -7,4 +7,15 @@ require_relative './exercise_4'
 puts "Exercise 5"
 puts "----------"
 
-# Your code goes here ...
+# Output for total revenue for all the stores
+p Store.sum("annual_revenue")
+
+# Output the average annual revenue
+p Store.average("annual_revenue")
+
+#Output the number of stores whose annual revenue is greater than 1M
+p Store.where("annual_revenue > ?", 1000000).count.to_s
+
+
+
+
