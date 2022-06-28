339fe4d65c5ef26d29c560176a9150d2415667aa
robinshrestha12 <robinshrestha.maple@gmail.com>
2022-06-28 18:17:27 -0400

    exercise_3 update

--- exercises/exercise_3.rb
+++ exercises/exercise_3.rb
@@ -5,4 +5,9 @@ require_relative './exercise_2'
 puts "Exercise 3"
 puts "----------"
 
-# Your code goes here ...
+@store3 = Store.find_by(id: 3) 
+
+@store3.destroy
+@store3.save
+puts Store.all.count
+
